71990d03986cd3e4db54ffe325555c3942251b5b
bala-amypo <bala@amypo.in>
2025-12-18 14:28:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,73 +1,49 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
 
-    @Column(unique=true)
     private String email;
-
-    @Column(nullable=false)
     private String password;
+    private String role;
 
-    private String role="STAFF";
-
-    public UserEntity(Long id, String name, String email, String password,String role) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password= password;
-        this.role=role;
-    }
-
-    public UserEntity() {
-        
-    }
-    public Long getId(){
+    
+    public Long getId() {
         return id;
     }
-    public void setId(Long id){
-        this.id=id;
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getEmail() {
-        return this.email;
+    public String getPassword() {
+        return password;
     }
-
-    public void setpasssword(String password) {
-        this.password =password;
+    
+    public void setPassword(String password) {
+        this.password = password;
     }
-
-    public String getpassword() {
-        return this.password;
+    
+    public String getRole() {
+        return role;
     }
-    public void setrole(String role) {
-        this.role=role;
-    }
-
-    public String getrole() {
-        return this.role;
+    
+    public void setRole(String role) {
+        this.role = role;
     }
 }
