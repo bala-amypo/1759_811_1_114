9321fe835d1816fd1df3942b25bed26cf92afaa4
bala-amypo <bala@amypo.in>
2025-12-18 14:40:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/QueueEntity.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class QueueEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private TokenEntity token;
+
+    private Integer position;
+    private LocalDateTime updatedAt;
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TokenEntity getToken() {
+        return token;
+    }
+
+    public void setToken(TokenEntity token) {
+        this.token = token;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
