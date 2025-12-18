85de0b8b5ff213ee429f5238e1e3e14b26ae3bc8
bala-amypo <bala@amypo.in>
2025-12-18 14:43:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/TokenLogRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.TokenLogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TokenLogRepository extends JpaRepository<TokenLogEntity, Long> {
+    List<TokenLogEntity> findByTokenId(Long tokenId);
+}
