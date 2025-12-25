965556f9cf2adac46e9b55af48c3f1e1df3a6f5a
bala-amypo <bala@amypo.in>
2025-12-25 13:06:53 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -4,7 +4,7 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import java.util.List;
-
+@service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
