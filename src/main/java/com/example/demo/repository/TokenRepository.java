abdc3e02828ef89ab31bf6d9b0b162cf7c84e9c9
bala-amypo <bala@amypo.in>
2025-12-25 15:29:38 +0000

    User commit

--- src/main/java/com/example/demo/repository/TokenRepository.java
+++ src/main/java/com/example/demo/repository/TokenRepository.java
@@ -1,13 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Token;
-import com.example.demo.entity.Token.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
-    List<Token> findByServiceCounter_IdAndStatusOrderByIssuedAtAsc(Long counterId, Status status);
+    List<Token> findByServiceCounter_IdAndStatusOrderByIssuedAtAsc(Long counterId, String status);
 
-    Token findByTokenNumber(String tokenNumber);
+    Optional<Token> findByTokenNumber(String tokenNumber);
 }
