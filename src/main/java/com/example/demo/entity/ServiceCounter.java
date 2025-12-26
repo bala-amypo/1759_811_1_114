5085515f20340ca53d6dad16b1a5a91584cb4f90
bala-amypo <bala@amypo.in>
2025-12-26 11:52:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ServiceCounter.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "service_counters")
+public class ServiceCounter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String counterName;
+
+    @Column(nullable = false)
+    private String department;
+
+    @Column(nullable = false)
+    private Boolean isActive = true;
+
+    // --------------------
+    // Getters & Setters
+    // --------------------
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCounterName() {
+        return counterName;
+    }
+
+    public void setCounterName(String counterName) {
+        this.counterName = counterName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean active) {
+        isActive = active;
+    }
+}
