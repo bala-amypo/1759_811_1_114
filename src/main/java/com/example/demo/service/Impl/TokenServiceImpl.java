f54aaab47caa366498573d9748f280d2f651f862
bala-amypo <bala@amypo.in>
2025-12-26 05:20:11 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TokenServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TokenServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.*;
+import com.example.demo.entity.*;
 import com.example.demo.repository.*;
 import com.example.demo.util.TokenNumberGenerator;
 import org.springframework.stereotype.Service;
