acbca95baeac6276120c1848c8d0653a7a4dd8bc
bala-amypo <bala@amypo.in>
2025-12-26 13:42:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/TokenLog.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class TokenLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Token token;
+
+    private String message;
+
+    private LocalDateTime loggedAt = LocalDateTime.now();
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Token getToken() { return token; }
+    public void setToken(Token token) { this.token = token; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public LocalDateTime getLoggedAt() { return loggedAt; }
+}
