6a595b25a4d578766ecaa6464d046df1564a3ace
bala-amypo <bala@amypo.in>
2025-12-26 15:11:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/QueuePositionRepository.java
+++ src/main/java/com/example/demo/repository/QueuePositionRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Queue;
+import com.example.demo.entity.QueuePosition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface QueueRepository extends JpaRepository<Queue, Long> {
+import java.util.List;
+
+@Repository
+public interface QueuePositionRepository extends JpaRepository<QueuePosition, Long> {
+    List<QueuePosition> findByQueueIdOrderByPositionAsc(Long queueId);
 }
