3a572b09c91e4e873105fd899b97912bd161d70f
bala-amypo <bala@amypo.in>
2025-12-26 12:53:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/QueuePosition.java
@@ -0,0 +1,40 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class QueuePosition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Token token;
+
+    private Integer position;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+}
